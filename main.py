e78b46b96a6af32afa2a29945a24e87e22d82a64
Hemant-1010 <hemantmeshram634@gmail.com>
2025-08-11 21:07:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from scrape import (scrape_website , split_dom_content, clean_body , extract_body )
+from parse import parse_with_ollama
+st.title("AI WEB SCRAPER")
+url = st.text_input("Enter a website URL : ")
+
+if st.button("Scrape Site"):
+    st.write("Scraping the website")
+    result = scrape_website(url)
+    body_content = extract_body(result)
+    cleaned_content = clean_body(body_content)
+
+
+    st.session_state.dom_content = cleaned_content
+    with st.expander("View DOM content"): 
+        st.text_area("Dom Content", cleaned_content , height= 300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse ?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the Content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks , parse_description)
+            st.write(result)        
+
+
